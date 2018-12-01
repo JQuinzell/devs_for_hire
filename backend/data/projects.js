190502d78f34894f0b18128a9ed45301539a5cd2
MrRainwater <n/a>
2018-12-01 17:36:26 -0600

    more projects

--- backend/data/projects.js
+++ backend/data/projects.js
@@ -6,6 +6,24 @@ module.exports = [
     developerIDs: ['jared', 'somebody'],
     available: true,
     dateMade: '12345',
-    description: 'Test project'
+    description: 'yeah boy this here be a neat project'
+  },
+  {
+    id: 'a',
+    title: 'Supa Cool',
+    managerID: '12',
+    developerIDs: ['jared', 'somebody'],
+    available: true,
+    dateMade: '12345',
+    description: 'yeah boy this here be a neat project'
+  },
+  {
+    id: 'b',
+    title: 'The Internet',
+    managerID: '12',
+    developerIDs: ['jared', 'somebody'],
+    available: true,
+    dateMade: '12345',
+    description: 'yeah boy this here be a neat project'
   }
 ]
