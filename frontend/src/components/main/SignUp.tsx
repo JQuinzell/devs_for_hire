cdfe5e178e53bf2806618c7d4099ecd972b307de
MrRainwater <n/a>
2018-12-01 15:30:55 -0600

    submit to backend

--- frontend/src/components/main/SignUp.tsx
+++ frontend/src/components/main/SignUp.tsx
@@ -4,72 +4,133 @@ import Grid from '@material-ui/core/Grid'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Checkbox from '@material-ui/core/Checkbox'
 import TextField from '@material-ui/core/TextField'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 
-interface FormValues {
-  isDeveloper: boolean
-  isProjectManajer: boolean
+const mutation = gql`
+  mutation createAccount($user: UserInput!) {
+    createAccount(user: $user) {
+      name
+      email
+      password
+      isProjectManager
+      isDeveloper
+      isMentor
+    }
+  }
+`
+interface UserInput {
   name: string
   email: string
   password: string
-  passwordConfirmation: string
+  isProjectManager: boolean
+  isDeveloper: boolean
+  isMentor: boolean
 }
 
-interface Props {}
+interface Data {
+  user: UserInput
+}
+
+class SignUpMutation extends Mutation<Data, Data> {}
 
 const { useState } = React
 
-const SignUp: React.FunctionComponent<Props> = ({}) => {
+const SignUp: React.FunctionComponent = ({}) => {
+  const [name, setName] = useState('')
   const [isDeveloper, setIsDeveloper] = useState(false)
+  const [isMentor, setIsMentor] = useState(false)
   const [isProjectManager, setisProjectManager] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   return (
-    <form>
-      <Grid container direction="column" alignContent="center" spacing={16}>
-        <Grid item>
-          <Grid item>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={isDeveloper}
-                  onChange={() => setIsDeveloper(!isDeveloper)}
-                />
+    <SignUpMutation mutation={mutation}>
+      {createAccount => (
+        <form
+          onSubmit={e => {
+            e.preventDefault()
+            createAccount({
+              variables: {
+                user: {
+                  name,
+                  isDeveloper,
+                  isMentor,
+                  isProjectManager,
+                  email,
+                  password
+                }
               }
-              label="Developer?"
-            />
-          </Grid>
-          <Grid item>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={isProjectManager}
-                  onChange={() => setisProjectManager(!isProjectManager)}
+            })
+          }}
+        >
+          <Grid container direction="column" alignContent="center" spacing={16}>
+            <Grid item>
+              <Grid item>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={isDeveloper}
+                      onChange={() => setIsDeveloper(!isDeveloper)}
+                    />
+                  }
+                  label="Developer?"
                 />
-              }
-              label="Project Manager?"
-            />
+              </Grid>
+              <Grid item>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={isProjectManager}
+                      onChange={() => setisProjectManager(!isProjectManager)}
+                    />
+                  }
+                  label="Project Manager?"
+                />
+              </Grid>
+              <Grid item>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={isMentor}
+                      onChange={() => setIsMentor(!isMentor)}
+                    />
+                  }
+                  label="Mentor?"
+                />
+              </Grid>
+            </Grid>
+            <Grid item>
+              <TextField
+                label="name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                label="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                label="password"
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <Button variant="contained" type="submit">
+                Submit
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid item>
-          <TextField
-            label="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            label="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </Grid>
-        <Grid item>
-          <Button variant="contained">Submit</Button>
-        </Grid>
-      </Grid>
-    </form>
+        </form>
+      )}
+    </SignUpMutation>
   )
 }
 
