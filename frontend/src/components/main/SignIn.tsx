a2c65cc7fd9639623233f34d588691ead8dcfea2
MrRainwater <n/a>
2018-12-01 12:41:58 -0600

    setup react router

---
+++ frontend/src/components/main/SignIn.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { createStyles, withStyles, WithStyles } from '@material-ui/core'
+
+const styles = createStyles({
+  container: {
+    height: '100%'
+  },
+  giantButton: {
+    width: 64 * 4,
+    height: 24 * 3
+  }
+})
+
+interface Props extends WithStyles<typeof styles> {}
+
+const SignIn: React.FunctionComponent<Props> = ({ classes }) => {
+  return (
+    <Grid
+      className={classes.container}
+      container
+      direction="column"
+      alignItems="center"
+      spacing={16}
+    >
+      <Grid item>
+        <Button
+          className={`${classes.giantButton}`}
+          variant="contained"
+          size="large"
+        >
+          Sign Up
+        </Button>
+      </Grid>
+      <Grid item>
+        <Typography variant="body1">or</Typography>
+      </Grid>
+      <Grid item>
+        <Button
+          className={`${classes.giantButton}`}
+          variant="contained"
+          size="large"
+        >
+          Sign In
+        </Button>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default withStyles(styles)(SignIn)
