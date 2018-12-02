6282559c170d794f73649452751d3df8a95ea211
Dylan Warren <duwtgb@mst.edu>
2018-12-01 20:50:34 -0600

    Funny users/projects

--- backend/data/projects.js
+++ backend/data/projects.js
@@ -1,29 +1,82 @@
 module.exports = [
   {
-    id: '12',
-    title: 'Some nice project',
-    managerID: '12',
-    developerIDs: ['somebody'],
+    id: 'z',
+    title: 'Hacker Day project',
+    managerID: 'dylan',
+    developerIDs: ['alice'],
     available: true,
-    dateMade: '12345',
-    description: 'yeah boy this here be a neat project'
+    dateMade: 'dylan345',
+    description: 'Our project for Local hacker day'
   },
   {
     id: 'a',
     title: 'Supa Cool',
-    managerID: '12',
-    developerIDs: ['somebody'],
+    managerID: 'dylan',
+    developerIDs: ['alice'],
     available: true,
-    dateMade: '12345',
+    dateMade: 'dylan345',
     description: 'yeah boy this here be a neat project'
   },
   {
     id: 'b',
     title: 'The Internet',
-    managerID: '12',
-    developerIDs: ['jared', 'somebody'],
+    managerID: 'dylan',
+    developerIDs: ['jared', 'alice', 'bob', 'garfield', 'dylan'],
     available: true,
-    dateMade: '12345',
-    description: 'yeah boy this here be a neat project'
+    dateMade: 'dylan345',
+    description: 'The best project, internet'
+  },
+  {
+    id: 'c',
+    title: 'Lasagna making robot',
+    managerID: 'garfield',
+    developerIDs: ['price', 'garfield'],
+    available: true,
+    dateMade: 'dylan345',
+    description: 'A robot to forever make lasagna for me'
+  },
+  {
+    id: 'd',
+    title: '1570 overhaul',
+    managerID: 'dylan',
+    developerIDs: [
+      'jared',
+      'alice',
+      'dylan',
+      'bob',
+      'garfield',
+      'karl',
+      'charlie'
+    ],
+    available: true,
+    dateMade: 'dylan345',
+    description: "Can we change this class yet, it's gotta be worked on"
+  },
+  {
+    id: 'e',
+    title: 'Alice and Bob foundation',
+    managerID: 'dylan',
+    developerIDs: ['bob', 'alice'],
+    available: true,
+    dateMade: 'dylan345',
+    description: 'Help us overtake the world with Alice and Bob'
+  },
+  {
+    id: 'f',
+    title: 'Dog removing bot',
+    managerID: 'garfield',
+    developerIDs: ['price', 'pepper', 'garfield'],
+    available: true,
+    dateMade: 'dylan345',
+    description: 'Gotta get Odie outta this house'
+  },
+  {
+    id: 'g',
+    title: "Lee's chicken self driving car",
+    managerID: 'dylan',
+    developerIDs: ['dylan', 'jared', 'karl'],
+    available: true,
+    dateMade: 'dylan345',
+    description: 'I just want easy access to chicken'
   }
 ]
--- backend/data/users.js
+++ backend/data/users.js
@@ -1,12 +1,12 @@
 module.exports = [
   {
-    id: '12',
+    id: 'dylan',
     name: 'Dylan Warren',
     email: 'duwtgb@mst.edu',
     password: 'hotdiggitydog',
     isMentor: false,
     isProjectManager: false,
-    projectIDs: ['a', 'b', '12'],
+    projectIDs: ['a', 'b', 'z'],
     languages: ['C++', 'TypeScript'],
     gitUrl: 'blah blah.com'
   },
@@ -22,14 +22,80 @@ module.exports = [
     gitUrl: 'blah blah.com'
   },
   {
-    id: 'somebody',
-    name: 'Somebody',
-    email: 'something@mst.edu',
-    password: 'hotdiggitydog',
+    id: 'alice',
+    name: 'Alice White',
+    email: 'alice@mst.edu',
+    password: 'ilikechicken',
+    isMentor: true,
+    isProjectManager: false,
+    projectIDs: ['b'],
+    languages: ['Python'],
+    gitUrl: 'github.com/alice'
+  },
+  {
+    id: 'bob',
+    name: 'Billy Bob',
+    email: 'bob@mst.edu',
+    password: 'b!ll7b0b',
     isMentor: false,
     isProjectManager: false,
-    projectIDs: ['12'],
-    languages: ['TypeScript'],
-    gitUrl: 'blah blah.com'
+    projectIDs: ['b'],
+    languages: ['Pearl'],
+    gitUrl: 'github.com/bobbyjr'
+  },
+  {
+    id: 'karl',
+    name: 'Karl Hayes',
+    email: 'karl@mst.edu',
+    password: 'packofciggies',
+    isMentor: false,
+    isProjectManager: true,
+    projectIDs: ['z'],
+    languages: ['gibberish'],
+    gitUrl: 'github.com/whatIsGit'
+  },
+  {
+    id: 'charlie',
+    name: 'Charlie the unicorn',
+    email: 'charlie@unicorn.me',
+    password: 'candyisland',
+    isMentor: false,
+    isProjectManager: false,
+    projectIDs: ['b'],
+    languages: ['unicorn'],
+    gitUrl: 'github.com/iHaveNoFingers'
+  },
+  {
+    id: 'garfield',
+    name: 'Garfield',
+    email: 'lasagna@cats.me',
+    password: 'lasagna',
+    isMentor: true,
+    isProjectManager: true,
+    projectIDs: ['b'],
+    languages: ['lasagna'],
+    gitUrl: 'github.com/garfield'
+  },
+  {
+    id: 'price',
+    name: 'Clayton Price',
+    email: 'price@mst.edu',
+    password: 'homerandcows',
+    isMentor: true,
+    isProjectManager: false,
+    projectIDs: ['b'],
+    languages: ['C++98'],
+    gitUrl: 'github.com/whoNeedsAComputer'
+  },
+  {
+    id: 'pepper',
+    name: 'Dr Pepper',
+    email: 'pepper@mst.edu',
+    password: 'ihatecola',
+    isMentor: true,
+    isProjectManager: false,
+    projectIDs: ['b'],
+    languages: ['bash'],
+    gitUrl: 'github.com/pepsiIsBad'
   }
 ]
